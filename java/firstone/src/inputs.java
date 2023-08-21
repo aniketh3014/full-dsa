479fd3c18aceb7420b6ada4e86eaca669556c772
Aniket <ghoshaniket1000@gmail.com>
2023-08-21 22:05:20 +0530

    taking user input

---
+++ java/firstone/src/inputs.java
@@ -0,0 +1,9 @@
+import  java.util.Scanner;
+public class inputs {
+    public static void main(String[] args){
+        System.out.println("please enter some input:");
+        Scanner scanner = new Scanner(System.in);
+        int rollno = scanner.nextInt();
+        System.out.println("your roll numer is " + rollno);
+    }
+}
