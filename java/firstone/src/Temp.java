e9543f6a822e39a57b4e2b8051ef6956d2404a4c
Aniket <ghoshaniket1000@gmail.com>
2023-08-30 07:14:34 +0530

    C to F temp converter

---
+++ java/firstone/src/Temp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Temp {
+    public static void main(String[] args) {
+        Scanner itemp = new Scanner(System.in);
+        System.out.println("please enter a temp in C:");
+        float ctemp = itemp.nextFloat();
+
+        float Ftemp = (ctemp * 9/5) + 32;
+        System.out.println(Ftemp);
+    }
+}
