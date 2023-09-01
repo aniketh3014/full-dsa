6dfbbb125f106d9a679a814e6ac9b2d4c908cd45
Aniket <ghoshaniket1000@gmail.com>
2023-09-01 07:08:05 +0530

    switch cases without break statements

--- java/switch/src/Main.java
+++ java/switch/src/Main.java
@@ -4,15 +4,23 @@ public class Main {
     public static void main(String[] args) {
         //take input and like mango and output "king of fruits", i can do it by many if statements but it will be very repetative.
         Scanner in = new Scanner(System.in);
-        String fruit = in.next();
+      //  String fruit = in.next();
 
         //new switch statement syntax:
-        switch (fruit) {
+     /*   switch (fruit) {
             case "mango" -> System.out.println("King of fruits");
             case "Apple" -> System.out.println("A nice red fruit");
             case "grapes" -> System.out.println("Small fruit");
             case "orange" -> System.out.println("good fruit");
             default -> System.out.println("please enter a valid fruit");
+
+
+        } */
+        int day = in.nextInt();
+
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("weekday");
+            case 6, 7 -> System.out.println("weekend");
         }
     }
 }
