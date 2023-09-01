798411fc07cd6a3ef9b81ccaca66e0b560410f0d
Aniket <ghoshaniket1000@gmail.com>
2023-09-01 08:36:19 +0530

    better way to write nested switch

--- java/switch/src/NestedSwitch.java
+++ java/switch/src/NestedSwitch.java
@@ -6,24 +6,17 @@ public class NestedSwitch {
 
         int Id = in.nextInt();
         String department = in.next();
-
+        // enhanced nested switch
         switch (Id) {
-            case 1:
+            case 1 -> {
                 System.out.println("employee no 1");
                 switch (department) {
-                    case "It":
-                        System.out.println("It department");
-                        break;
-                    case "CSE":
-                        System.out.println("CSE department");
-                        break;
-                    default:
-                        System.out.println("enter correct Department!");
-
+                    case "It" -> System.out.println("It department");
+                    case "CSE" -> System.out.println("CSE department");
+                    default -> System.out.println("enter correct Department!");
                 }
-                break;
-            default:
-                System.out.println("enter correct Id");
+            }
+            default -> System.out.println("enter correct Id");
         }
 
     }
