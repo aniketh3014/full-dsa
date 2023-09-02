b2f523f2354a9d0ae326fd2d98f4200d1e917024
Aniket <ghoshaniket1000@gmail.com>
2023-09-02 09:01:28 +0530

    normal way to sum

---
+++ java/Methods/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Q take input of 2 numbers and print the sum
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
