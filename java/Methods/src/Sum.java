0f54d59e516bd257b4c8fa958d31d435d8fca21d
Aniket <ghoshaniket1000@gmail.com>
2023-09-02 11:55:19 +0530

    using arguments with  methods

--- java/Methods/src/Sum.java
+++ java/Methods/src/Sum.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
-        sum();
-
+       int x = sum2(122, 122);
+        System.out.println(x);
     }
     /*
     return_type name () {
         //body
-        return statement;
+        return statement; //it will be void if the method does not return anything
        }
      */
-    static void sum() {
+    static int sum1() {
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         int num3 = num1+num2;
-        System.out.println("the sum is " + num3);
+        return num3;
+    }
+    static int sum2(int a, int b) {
+        return a+b;
     }
 }
