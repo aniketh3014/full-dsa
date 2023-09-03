2a07fb5c98361bfeeeae8d4111de5562bc8771ef
Aniket <ghoshaniket1000@gmail.com>
2023-09-03 12:29:22 +0530

    added more comments in overloading for better understanding

--- java/Methods/src/Overloading.java
+++ java/Methods/src/Overloading.java
@@ -1,4 +1,4 @@
-public class Overloading {
+public class Overloading { // all this happens at compile time
     public static void main(String[] args) {
         ag(67); // it will automatically call the right function based on the values passed in
         ag("aniket");
@@ -6,12 +6,15 @@ public class Overloading {
     }
     // multiple functions can with same name can be used but with different arguments
     static void ag(int x) {
+        System.out.println("first one");
         System.out.println(x);
     }
     static void ag(String name) {
+        System.out.println("second one");
         System.out.println(name);
     }
-    static void ag(int y, int z) { //same type but different args
+    static void ag(int y, int z) {
+        System.out.println("third one");//same type but different args
         System.out.println(y+z);
     }
 }
