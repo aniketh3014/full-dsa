bf67282c47204192bb52da69c19f10456179e871
Aniket <ghoshaniket1000@gmail.com>
2023-09-03 13:03:18 +0530

    now it outputs all the 3digit am numbers

--- java/Methods/src/Question.java
+++ java/Methods/src/Question.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Question {
     public static void main(String[] args) {
         //print all the three digit aramstrong numbers
-        Scanner in = new Scanner(System.in);
-        System.out.print("enter a number to check: ");
-        int n = in.nextInt();
-        System.out.println(armstrong(n)); // prints is true or false
+       // Scanner in = new Scanner(System.in);
+       // System.out.print("enter a number to check: ");
+        //int n = in.nextInt();
+        // System.out.println(armstrong(n)); // prints is true or false
 
+        // printing all the 3digit am numbers
+        for (int i = 100; i < 1000; i++) {
+            if (armstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
     static boolean armstrong(int num) {
         int orig = num;
