4779981b3555971827685e353a083ceda881a2c3
Aniket <ghoshaniket1000@gmail.com>
2023-09-03 12:57:10 +0530

    modified question to take user input

--- java/Methods/src/Question.java
+++ java/Methods/src/Question.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Question {
     public static void main(String[] args) {
         //print all the three digit aramstrong numbers
-        int n =153;
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter a number to check: ");
+        int n = in.nextInt();
         System.out.println(armstrong(n)); // prints is true or false
 
     }
