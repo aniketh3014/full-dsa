4e65b9138f64fca7b846ff6b7432c50affca2f53
Aniket <ghoshaniket1000@gmail.com>
2023-09-04 09:18:09 +0530

    creating basic arrays and added notes

---
+++ java/Arrays/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // array syntax
+        // datatype[] variable_name = new datatype[size];
+
+        //store 5 roll numbers:
+        int[] roll = new int[5];
+        // or do it directly
+        int[] roll2 = {1,2,3,4,5};
+
+        int[] roll3; // declaration , getting defined into stack memory.
+        roll3 = new int[5]; // initialisation, here object is being created in the heap memory.
+        System.out.println(roll3[1]);
+    }
+}
