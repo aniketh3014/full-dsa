3a980aab11afb1a8f6fec458ae23baab4ea454fb
Aniket <ghoshaniket1000@gmail.com>
2023-09-06 07:22:57 +0530

    looping thorough an array with enhanced for loop

---
+++ java/Arrays/src/Input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[4];
+        for (int i =0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int j : arr) {
+            System.out.print(j + " "); // here mun represents the elements of the array
+        }
+
+    }
+}
