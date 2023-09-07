87790e2f47f34c3e57f09b58150337c993ed92d9
Aniket <ghoshaniket1000@gmail.com>
2023-09-07 07:29:00 +0530

    creating 2d array

--- java/Arrays/src/Input.java
+++ java/Arrays/src/Input.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] arr = new int[4];
+       /* int[] arr = new int[4];
         for (int i =0; i < arr.length; i++) {
             arr[i] = in.nextInt();
         }
         for (int j : arr) {
             System.out.println(j + " "); // here mun represents the elements of the array
-        }
+        }*/
 
         // easy and the best way to print an array
-        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
+
+        // array of objects
+        String[] str = new String[4];
+
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+        System.out.println(Arrays.toString(str));
 
     }
 }
---
+++ java/Arrays/src/Multidimentional.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Multidimentional {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new  int[3][3];
+        /*int[][] arr = {
+                {1, 2, 3}, //0th index
+                 {4, 5},
+                {7, 8, 9}
+        };*/
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
