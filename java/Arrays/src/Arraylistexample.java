72c1b0ecf0cf309932ad5523ff9812e8d7904168
Aniket <ghoshaniket1000@gmail.com>
2023-09-12 14:01:44 +0530

    using arraylist to store array of unknown length

---
+++ java/Arrays/src/Arraylistexample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Arraylistexample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(2);
+        list.add(4354);
+        list.add(654);
+        list.add(8748);
+        list.add(867);
+        System.out.println(list);
+    }
+}
