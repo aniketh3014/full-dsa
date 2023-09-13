589c03e81afa2e27c1b5bfe4faa289a170668c55
Aniket <ghoshaniket1000@gmail.com>
2023-09-13 08:37:42 +0530

    using loops to fill the array list and acccessing the list elements

--- java/Arrays/src/Arraylistexample.java
+++ java/Arrays/src/Arraylistexample.java
@@ -1,16 +1,31 @@
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Arraylistexample {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(2);
-        list.add(4354);
-        list.add(654);
-        list.add(8748);
-        list.add(867);
-        list.remove(1);
-        System.out.println(list.contains(654));
-        list.set(0, 100);
-        System.out.println(list);
+        Scanner in = new Scanner(System.in);
+         ArrayList<Integer> list = new ArrayList<>(2);
+//        list.add(4354);
+//        list.add(654);
+//        list.add(8748);
+//        list.add(867);
+//        list.remove(1);
+//        System.out.println(list.contains(654));
+//        list.set(0, 100);
+//        System.out.println(list);
+
+        // taking input to fill the array list
+        for (int i =0; i < 3; i++) {
+            list.add(in.nextInt());
+        }
+
+        // accessing the elements by for loops
+        for (int i = 0; i < 3; i++) {
+            System.out.println(list.get(i));
+        }
+       // System.out.println(list);
+
 
     }
 }
