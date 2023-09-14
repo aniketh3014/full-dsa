21d1c76b377f9ea9f627d31aef0c10a5719d987f
Aniket <ghoshaniket1000@gmail.com>
2023-09-14 07:59:41 +0530

    finding the max value in a provided range

--- java/Arrays/src/Max.java
+++ java/Arrays/src/Max.java
@@ -1,7 +1,18 @@
 public class Max {
     public static void main(String[] args) {
         int[] arr = {1, 2 , 3, 4, 5, 6, 7};
-        System.out.println(max(arr));
+        System.out.println(maxinRange(arr, 1, 3));
+    }
+
+    // finding a maximum value in a range
+    static int maxinRange(int[] arr, int start, int end) {
+        int maxval = arr[start];
+        for (int i = 0; i <= end; i++) {
+            if (arr[i] > maxval) {
+                maxval = arr[i];
+            }
+        }
+        return maxval;
     }
     static int max(int[] arr) {
         int maxval = arr[0];
