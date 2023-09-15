eee93e2825a0d08fa774e313f5a63b6cc6530313
Aniket <ghoshaniket1000@gmail.com>
2023-09-15 08:51:30 +0530

    replaced for loop with enhanced for loop, and removed if statement

--- java/Linear-search/src/Main.java
+++ java/Linear-search/src/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {45, 3, 43, 5, 52, -10, 23, 88, 100, 10};
-        int target = 88;
+        int target = -10;
         int ans = linearsearch(arr, target);
         System.out.println(ans);
     }
     //search in array: return the index if item found
     // otherwise if item not found return -1;
     static int linearsearch(int[] arr, int target) {
-        if (arr.length == 0) {
-            return -1;
-        }
         // run a for loop
-        for (int index = 0; index < arr.length; index++) {
+        for (int element : arr) {
             // check for element at every index
-            int element = arr[index];
             if (element == target) {
-                return index;
+                return element;
             }
         }
         // if not the element does not exist in the array
