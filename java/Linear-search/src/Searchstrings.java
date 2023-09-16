ff9ad76529365e43219d5e79bbc11e8a7f86298e
Aniket <ghoshaniket1000@gmail.com>
2023-09-16 07:55:44 +0530

    doing the same but using enhanced for loop with 'toCharArray'

--- java/Linear-search/src/Searchstrings.java
+++ java/Linear-search/src/Searchstrings.java
@@ -1,18 +1,31 @@
 public class Searchstrings {
     public static void main(String[] args) {
         String name = "aniket";
-        char target = 'e';
-        System.out.println(search(name, target));
+        char target = 'x';
+        System.out.println(search2(name, target));
     }
-    static boolean search(String str, char target) {
-        if  (str.isEmpty()) {
+
+    static boolean search1(String str, char target) {
+        if (str.isEmpty()) {
             return false;
         }
         for (int i = 0; i < str.length(); i++) {
             if (target == str.charAt(i)) {
                 return true;
+            }
         }
+        return false;
+    }
+    static boolean search2(String str, char target) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
+                return true;
+            }
         }
         return false;
     }
 }
+
