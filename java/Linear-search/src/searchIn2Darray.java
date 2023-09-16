365800ffadebcbf2536643997a0129d11df98430
Aniket <ghoshaniket1000@gmail.com>
2023-09-16 09:28:39 +0530

    ** searching inside a 2d array

---
+++ java/Linear-search/src/searchIn2Darray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class searchIn2Darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23, 342, 24, 243},
+                {66, 55, 22},
+                {11, 12, 13, 14},
+                {77, 88, 99, 10}
+        };
+        int target = 23;
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
