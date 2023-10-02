b1796043aeb1fcbf66b2f5898295e9050fe6f693
Aniket Ghosh <ghoshaniket1000@gmail.com>
2023-10-02 07:56:07 +0530

    added checks if the array is already sorted then break the loop

--- java/Shorting/src/Main.java
+++ java/Shorting/src/Main.java
@@ -2,13 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {12, 1, 23, 234, 8, 2};
+        int[] arr = {1, 2, 3, 4, 5, 6};
         bubbleShort(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void bubbleShort(int[] arr) {
+        boolean swapped;
         //run the steps n-1 times
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             // for each step, max element will come at the last index
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the item is smaller than the previous item
@@ -17,7 +19,11 @@ public class Main {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
