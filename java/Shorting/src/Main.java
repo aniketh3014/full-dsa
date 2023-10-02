f2c6aa15c1116dec1a272825bc3044627be0d998
Aniket Ghosh <ghoshaniket1000@gmail.com>
2023-10-02 08:00:23 +0530

    testing diferent cases

--- java/Shorting/src/Main.java
+++ java/Shorting/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {-1, 32, -32, 0, 88, 100};
         bubbleShort(arr);
         System.out.println(Arrays.toString(arr));
     }
